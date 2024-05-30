87eedf5b3b293a5fd55a24984d16aec03816f16a
starboy011 <rishavofficial294@gmail.com>
2024-05-30 19:11:35 +0100

    added docker configuration

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
