1da57c49d7a304799fe56429179cd8f82ef225f0
starboy011 <rishavofficial294@gmail.com>
2024-05-30 19:26:20 +0100

    2DChanged

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!!")
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
 
 	http.HandleFunc("/", helloHandler)
 
-	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting server on :4040")
+	if err := http.ListenAndServe(":4040", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
