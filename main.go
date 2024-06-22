cab73f4357d4178920a974729f537def6081feb6
starboy011 <rishavofficial294@gmail.com>
2024-06-22 08:24:45 +0100

    removed comments

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	// Initialize New Relic agent
+
 	app, err := newrelic.NewApplication(
 		newrelic.ConfigAppName("api-gateway"),
 		newrelic.ConfigLicense("eu01xxc12dc4fe3729b337cc4130261eFFFFNRAL"),
@@ -20,22 +20,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Create a mux router
 	router := mux.NewRouter()
 
-	// Setup your routes
 	server.SetupBarberShopsServiceRoutes(router)
 
-	// Instrument the handler with New Relic
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		txn := app.StartTransaction(req.URL.Path)
 		defer txn.End()
 
-		// Route requests through the mux router
 		router.ServeHTTP(w, req)
 	})
 
-	// Start your HTTP server
 	log.Println("Listening on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
